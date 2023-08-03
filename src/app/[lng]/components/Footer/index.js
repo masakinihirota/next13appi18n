7e51acffa57a8880df00da70cd5ea7aea63168eb
masakinihirota <masakinihirota@gmail.com>
2023-08-04 01:44:01 +0900

    🐛  ESlintのエラーの対処

--- src/app/[lng]/components/Footer/index.js
+++ src/app/[lng]/components/Footer/index.js
@@ -4,11 +4,14 @@ import { languages } from "../../../i18n/settings"
 import { useTranslation } from "../../../i18n"
 
 export const Footer = async ({ lng }) => {
+  // awaitが使えないとあるが、Next.js 13 ではサーバーサイドでawaitが使えるようになった
+  // 現在このページはサーバーサイドなのでエラーが出るのは間違い。無効化する。
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { t } = await useTranslation(lng, "footer")
   return (
     <footer style={{ marginTop: 50 }}>
       <Trans i18nKey="languageSwitcher" t={t}>
-        Switch from <strong>{{ lng }}</strong> to:{" "}
+        対応言語変更スイッチ <strong>{{ lng }}</strong> to:{" "}
       </Trans>
       {languages
         .filter((l) => lng !== l)
--- src/app/[lng]/second-page/page.js
+++ src/app/[lng]/second-page/page.js
@@ -3,6 +3,9 @@ import { useTranslation } from "../../i18n"
 import { Footer } from "../components/Footer"
 
 export default async function Page({ params: { lng } }) {
+  // awaitが使えないとあるが、Next.js 13 ではサーバーサイドでawaitが使えるようになった
+  // 現在このページはサーバーサイドなのでエラーが出るのは間違い。無効化する。
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { t } = await useTranslation(lng, "second-page")
   return (
     // <>
